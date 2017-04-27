9618f57987d18a48c75b06b9e75251b4f6177fd1
imehrdad2012 <imehrdad2012@gmail.com>
2017-04-27 00:18:58 -0400

    modified

--- test/test-mehrdad.lua
+++ test/test-mehrdad.lua
@@ -1,11 +1,26 @@
-package.path = package.path ..";?.lua;test/?.lua;app/?.lua;"
+function write_stats_to_file(log_file, sendport, rcvport, time)
+    local file = io.open(log_file, "w");
+    local stats = pktgen.portStats(sendport..","..rcvport, "port");
+    local sentpkts = stats[tonumber(sendport)].opackets;
+    local rcvdpkts = stats[tonumber(rcvport)].ipackets;
 
-delay = 4000
+    file:write(string.format("\n*** RESULT:\tFinished in\t\t%d seconds\n", time));
+    file:write(string.format("\t\tsent %d pkts\t%.4f Mpkts/sec\n", sentpkts, (sentpkts/1000000)/time));
+    file:write(string.format("\t\trcvd %d pkts\t%.4f Mpkts/sec\n", rcvdpkts, (rcvdpkts/1000000)/time));
+    file:write(string.format("\t\tdeltapkts(TX - RX):\t%d pkts\n***\n", sentpkts-rcvdpkts));
 
-for i=100, 1500, 200
-do
-    pktgen.set("all", "size", i);
-    pktgen.start("all");
-    pktgen.delay(delay);
-    pktgen.stop("all");
+    file:write(string.format("Pktgen-DPDK port %d dump:", sendport), "\n\n");
+    -- file:write(serialize("linkState", pktgen.linkState(sendport)));
+    -- file:write(serialize("isSending", pktgen.isSending(sendport)));
+    file:write(serialize("portSizes", pktgen.portSizes(sendport)));
+    file:write(serialize("pktStats", pktgen.pktStats(sendport)));
+    file:write(serialize("portRates", pktgen.portStats(sendport, "rate")));
+    file:write(serialize("portStats", pktgen.portStats(sendport, "port")));
+
+    file:close();
+end
+
+
+function os_sleep(n)
+  os.execute("sleep " .. tonumber(n))
 end
